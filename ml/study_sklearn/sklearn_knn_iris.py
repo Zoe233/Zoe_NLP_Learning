bd87fb211d88665bf63a7c6ef4555b58a9ef5ab7
Zoe <740465595@qq.com>
2018-05-18 14:51:55 +0800

    新增sklearn常用方法datasets,model,cross validation等内容的总结和samples

---
+++ ml/study_sklearn/sklearn_knn_iris.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+from sklearn import neighbors
+from sklearn import datasets
+
+# 加载iris公开数据集
+iris = datasets.load_iris()
+
+# save data
+# f = open("iris.data.csv", 'wb')
+# f.write(str(iris))
+# f.close()
+
+# 生成KNN分类器实例
+knn = neighbors.KNeighborsClassifier()
+
+# 加载训练集
+knn.fit(iris.data, iris.target)
+
+# 验证单条数据
+predictedLabel = knn.predict([[0.1, 0.2, 0.3, 0.4]])
+
+print ("predictedLabel is :", predictedLabel)
